77f6202327babdbedb18fc4efc2b5bf92b72659e
Amos Babu <amos.babu@yahoo.com>
2025-03-27 13:15:24 +0300

    about us

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,4 +1,6 @@
 import BackgroundImage from "../components/BackgroundImage";
+import checkCircle from "../assets/check-circle.svg";
+import whyChoose from "../data/whychoose.json";
 
 const AboutUs = () => {
   return (
@@ -15,8 +17,24 @@ const AboutUs = () => {
           projects.
         </p>
       </div>
-      <div className="font-semibold text-xl text-center lg:text-left md:text-left lg:mx-5 md:mx-5 mt-10 mb-20 lg:mb-10">
+      <div className="font-semibold text-4xl text-center lg:mx-5 md:mx-5 mt-10 ">
         Why Choose Vinray Engineering Limited?
+      </div>
+      <div className="flex flex-col justify-center lg:justify-start lg:mx-4 mt-10 mb-20 gap-4">
+        {whyChoose.map((why) => (
+          <div
+            key={why.id}
+            className="flex flex-col gap-4 justify-center lg:mx-80"
+          >
+            <div className="flex justify-center gap-4">
+              <img className="w-10 h-10" src={checkCircle} alt="Check" />
+              <p className="self-center text-xl font-medium">{why.name}</p>
+            </div>
+            <div className="text-lg text-center self-center text-neutral-500">
+              {why.description}
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );

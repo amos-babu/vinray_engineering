11962b4c9941483a73a1fa4fb3266dc989b5f9e9
Amos Babu <amos.babu@yahoo.com>
2025-03-28 13:26:46 +0300

    implementing products page

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -6,48 +6,48 @@ import envelope from "../assets/envelope.svg";
 const ContactUs = () => {
   return (
     <>
-      <BackgroundImage header="Contacts" />
-      <div className="mb-20">
-        <h1 className="font-bold text-4xl lg:text-6xl text-center">
+      <BackgroundImage header="Contact Us" />
+      <div className="mb-10 lg:mb-20">
+        <h1 className="font-bold text-2xl lg:text-4xl text-center">
           Get In Touch
         </h1>
       </div>
 
       <div className="flex flex-col justify-center items-center mb-20">
         <div
-          className="block w-full lg:w-3/4 cursor-pointer bg-gray-100 border
+          className="block w-5/6 lg:w-3/4 cursor-pointer bg-gray-100 border
              border-gray-200 rounded-lg shadow-sm 
              hover:bg-orange-100 dark:bg-gray-800 dark:border-gray-700
               dark:hover:bg-gray-700"
         >
-          <h1 className="font-bold text-2xl lg:text-6xl text-center py-3">
+          <h1 className="font-bold text-xl lg:text-2xl text-center py-3">
             Nairobi Office
           </h1>
 
           <div className="flex flex-col lg:flex-row gap-4 mt-10 justify-center lg:justify-start lg:px-50 items-center">
             <img className="w-10 h-10" src={mapPin} alt="map-pin" />
-            <p className="text-xl font-semibold text-center lg:text-start py-3">
+            <p className="text-base font-semibold text-center lg:text-start py-3">
               Ruiru Town P.O. Box 33319-00600, Nairobi, Kenya
             </p>
           </div>
 
           <div className="flex flex-col lg:flex-row gap-4 mt-10 justify-center lg:justify-start lg:px-50 items-center">
             <img className="w-10 h-10" src={envelope} alt="envelope" />
-            <p className="text-xl font-semibold break-all text-center lg:text-start py-3">
+            <p className="text-base font-semibold break-all text-center lg:text-start py-3">
               sales@vinrayengineering.com
             </p>
           </div>
 
           <div className="flex flex-col lg:flex-row gap-4 mt-10 justify-center lg:justify-start lg:px-50 items-center">
             <img className="w-10 h-10" src={envelope} alt="envelope" />
-            <p className="text-xl break-all font-semibold text-center lg:text-start py-3">
+            <p className="text-base break-all font-semibold text-center lg:text-start py-3">
               info@vinrayengineering.com
             </p>
           </div>
 
           <div className="flex flex-col lg:flex-row gap-4 mt-10 justify-center lg:justify-start lg:px-50 items-center">
             <img className="w-10 h-10" src={phone} alt="phone" />
-            <p className="text-xl font-semibold text-center lg:text-start py-3">
+            <p className="text-base font-semibold text-center lg:text-start py-3">
               +254756020125, +254756020132, +254756020134
             </p>
           </div>
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,38 +1,38 @@
+import { useState } from "react";
 import BackgroundImage from "../components/BackgroundImage";
+import projects from "../data/projects.json";
 
 const Products = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   return (
     <>
       <BackgroundImage header="Products" />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mx-5">
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-            alt=""
-          />
-        </div>
+      <div className="flex flex-wrap justify-center items-center mt-10 px-4 gap-4">
+        {projects.map((project) => (
+          <button
+            key={project.id}
+            onClick={() => setSelectedCategory(project.category)}
+            className={`text-sm cursor-pointer font-semibold bg-black/10 hover:bg-[#EF7275]
+             hover:text-white py-3 w-50 text-center rounded-full transition-all`}
+          >
+            {project.category}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mx-5 mt-10">
+        {selectedCategory &&
+          projects
+            .find((p) => p.category === selectedCategory)
+            ?.images.map((image) => (
+              <div key={image.id}>
+                <img
+                  className="h-auto max-w-full rounded-lg"
+                  src={image.src}
+                  alt="Products"
+                />
+              </div>
+            ))}
       </div>
     </>
   );

d6a369c4d9edb45dbe26c5debb092cc275d5b591
amos-babu <amos.babu@yahoo.com>
2025-06-19 12:10:24 +0300

    running tests

---
+++ src/tests/ContactForm.test.ts
@@ -0,0 +1,14 @@
+import { it, expect, describe } from 'vitest'
+import ContactForm from "../components/ContactForm";
+
+describe(ContactForm, () => {
+    it("Displays Correctly initial count", () => {
+
+    })
+})
+
+describe('group', () => {
+    it('should', () => {
+        expect(1).toBeTruthy();
+    })
+})

c4c13f5bae87728e4d4dcdb5bed2ea49bea4d8d6
amos-babu <amos.babu@yahoo.com>
2025-07-19 12:52:18 +0300

    buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,17 +22,18 @@ const Home = () => {
 
   return (
     <div className="flex flex-col relative h-screen w-full overflow-hidden">
-      <div className="absolute inset-0 bg-black/60 z-[1]"></div> 
+      <div className="absolute inset-0 bg-black/60 z-[1]"></div>
       <Navbar />
       <video
         className="absolute top-1/2 left-1/2 min-w-full 
         min-h-full object-cover -translate-x-1/2 
-        -translate-y-1/2 z-0" 
-        src={videoBg} 
+        -translate-y-1/2 z-0"
+        src={videoBg}
         aria-label="Background Video"
-        autoPlay 
-        loop 
-        muted  />
+        autoPlay
+        loop
+        muted
+      />
       <div
         id="content"
         className="relative z-10 flex flex-col items-center 
@@ -46,18 +47,23 @@ const Home = () => {
           leading-13 md:leading-14 lg:leading-30 text-white md:text-5xl 
           lg:text-8xl dark:text-white"
         >
-          We <span className="underline decoration-orange-600 underline-offset-10">engineer</span>{" "}
+          We{" "}
+          <span className="underline decoration-orange-600 underline-offset-10">
+            engineer
+          </span>{" "}
           the{" "}
-          <span className="bg-gradient-to-r from-orange-600 to-orange-700 bg-clip-text 
-          text-transparent">
+          <span
+            className="bg-gradient-to-r from-orange-600 to-orange-700 bg-clip-text 
+          text-transparent"
+          >
             future’s{" "}
-            <span className="underline decoration-white underline-offset-10">possibilities</span>
+            <span className="underline decoration-white underline-offset-10">
+              possibilities
+            </span>
           </span>
         </motion.h1>
 
-        <span
-          className="mx-3 text-center text-xl font-extralight w-2/3"
-        >
+        <span className="mx-3 text-center text-xl font-extralight w-2/3">
           The services we provide are a fusion of our knowledge, skills, and
           expertise, combined with a deep understanding of our customers’ needs
           and requirements.
@@ -82,7 +88,7 @@ const Home = () => {
               initial="hidden"
               animate="visible"
               className="cursor-pointer px-10 mt-3 py-3 text-base md:text-lg border pointer-cursor 
-              hover:text-orange-600 rounded-3xl"
+              hover:text-orange-600 hover:bg-gray-100 hover:border-orange-600 rounded-3xl"
             >
               Learn More
             </motion.button>
